d2cc4f26d5898fee622d10e3875b2f8cda4164f2
Vojtěch Bojda <info@vojtechbojda.cz>
2024-01-14 19:07:49 +0100

    improves fixUtf8

--- src/Utils/StringHelper.php
+++ src/Utils/StringHelper.php
@@ -4,8 +4,14 @@ namespace Edisk\Common\Utils;
 
 class StringHelper
 {
+    /** @noinspection PhpRedundantOptionalArgumentInspection */
     public static function fixUtf8(string $value): string
     {
-        return iconv('UTF-8', 'UTF-8//IGNORE', $value);
+        $encoding = mb_detect_encoding($value, mb_detect_order(), false);
+        if ($encoding === 'UTF-8') {
+            $value = mb_convert_encoding($value, 'UTF-8', 'UTF-8');
+        }
+
+        return iconv(mb_detect_encoding($value, mb_detect_order(), false), 'UTF-8//IGNORE', $value);
     }
 }
